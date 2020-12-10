2c6652ff34faaad3d877752cb24496958a71c394
Alexandru Nechita <alexandru.italia32@gmail.com>
2020-12-10 00:27:30 +0000

    Fix ping command

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on('message', async message => {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ws.ping)}ms`);
   }
 
 });
